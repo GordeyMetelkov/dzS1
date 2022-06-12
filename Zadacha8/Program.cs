46e8b11aaffaeed9d5acfefe47c1bae2e9e5126e
Gordey <gordeymetelkov@icloud.com>
2022-06-12 16:08:50 +0300

    init

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+if (N == 0)
+{
+    Console.WriteLine("Вы ввели 0, нет четных чисел в промежутке");
+}
+if (N > 0)
+{
+    Console.WriteLine("Все четные числа:"); 
+    while (a <= N)
+    {
+        if (a%2 > 0)
+        {
+            a++;
+        }
+        else
+        {
+            Console.Write(a);
+            Console.Write(" ");
+            a = a + 2; 
+        }
+    }
+}
+if (N < 0)
+{
+    Console.WriteLine("Все четные числа:");
+    while (Math.Abs(a) <= Math.Abs(N))
+    {
+        if (a%2 > 0)
+        {
+            a = a + 1;
+        }
+        else
+        {
+            Console.Write(-a);
+            Console.Write(" ");
+            a = a + 2; 
+        }
+    }
+}
